9956f0608d0decf7ca4d76f8468d91d431a24b3f
razuberiii <razuberi@163.com>
2023-06-06 00:45:13 +0800

    code update

--- comic.py
+++ comic.py
@@ -17,131 +17,127 @@ import driver as dr
 
 driver = dr.driver
 
-find = input("漫画名字:")
-# 设置一个等待时间
-wait = WebDriverWait(driver, 20)
-regex_for_comic = re.compile(f"href.*?(?={find})")
 
-JS1 = "return document.querySelector(\"div[data-index='1'] canvas\").toDataURL('image/png');"
-JS0 = "return document.querySelector(\"div[data-index='0'] canvas\").toDataURL('image/png');"
-JS2 = "return document.querySelector(\"div[data-index='2'] canvas\").toDataURL('image/png');"
+class Comic():
+    def __init__(self, name, episode):
+        self.episode = episode
+        self.find = name
+        # 设置一个等待时间
+        self.wait = WebDriverWait(driver, 20)
+        self.regex_for_comic = re.compile(f"href.*?(?={self.find})")
 
-offset_is_set = False
+        self.JS1 = "return document.querySelector(\"div[data-index='1'] canvas\").toDataURL('image/png');"
+        self.JS0 = "return document.querySelector(\"div[data-index='0'] canvas\").toDataURL('image/png');"
+        self.JS2 = "return document.querySelector(\"div[data-index='2'] canvas\").toDataURL('image/png');"
 
-driver.set_window_size(626, 1000)
+        self.offset_is_set = False
 
+        driver.set_window_size(626, 1000)
 
-def load_source(episode):
-    url = 'https://comic-walker.com/'
-    driver.get(url)
-    search_box = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR,
-                                                            '#searchFormHeader .keywordBox')))
-    search_box.clear()
-    search_box.send_keys(f"{find}")
-    search_box.send_keys(Keys.ENTER)
-    wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, ".tileList")))
-    tile_list = driver.find_elements(By.CSS_SELECTOR, '.tileList h2')
-    link_list = driver.find_elements(By.CSS_SELECTOR, '.tileList a')
-    link = ''
-    for i in range(0, len(tile_list)):
-        if re.search(f"{find}", tile_list[i].text):
-            link = link_list[i].get_attribute("href")
-            break
-    driver.get(link)
-    try:
-        wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.ac-trigger-btn-more'))).click()
-    except:
-        raise
-    book_list = wait.until(
-        ec.presence_of_all_elements_located((By.CSS_SELECTOR,
-                                             '#backnumberComics .ac-content .container a')))
+    def load_source(self, episode):
+        url = 'https://comic-walker.com/'
+        driver.get(url)
+        search_box = self.wait.until(ec.presence_of_element_located((By.CSS_SELECTOR,
+                                                                     '#searchFormHeader .keywordBox')))
+        search_box.clear()
+        search_box.send_keys(f"{self.find}")
+        search_box.send_keys(Keys.ENTER)
+        self.wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, ".tileList")))
+        tile_list = driver.find_elements(By.CSS_SELECTOR, '.tileList h2')
+        link_list = driver.find_elements(By.CSS_SELECTOR, '.tileList a')
+        link = ''
+        for i in range(0, len(tile_list)):
+            if re.search(f"{self.find}", tile_list[i].text):
+                link = link_list[i].get_attribute("href")
+                break
+        driver.get(link)
+        try:
+            self.wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, '.ac-trigger-btn-more'))).click()
+        except:
+            raise
+        book_list = self.wait.until(
+            ec.presence_of_all_elements_located((By.CSS_SELECTOR,
+                                                 '#backnumberComics .ac-content .container a')))
 
-    click_enabled(0, book_list[int(episode) - 1])
+        self.click_enabled(0, book_list[int(episode) - 1])
 
+    def get_source(self, episode):
+        count = 1
+        print(driver.current_url)
+        self.wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, r"canvas")))
+        time.sleep(5)
+        total_page = self.get_total_page()
+        actions = self.setup_offset()
+        print(total_page)
+        for i in range(1, total_page - 1):
+            if count == 0:
+                js = self.JS0
+                count = 2
+            elif count == 1:
+                js = self.JS1
+                count = 0
+            elif count == 2:
+                js = self.JS2
+                count = 1
 
-def get_source(episode):
-    count = 1
-    print(driver.current_url)
-    wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, r"canvas")))
-    time.sleep(5)
-    total_page = get_total_page()
-    actions = setup_offset()
-    print(total_page)
-    for i in range(1, total_page - 1):
-        if count == 0:
-            js = JS0
-            count = 2
-        elif count == 1:
-            js = JS1
-            count = 0
-        elif count == 2:
-            js = JS2
-            count = 1
+            print(js, count)
 
-        print(js, count)
+            img_bytes = self.decode_base64(js, 0)
+            self.wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, r"body")))
+            self.next_page(actions)
 
-        img_bytes = decode_base64(js, 0)
+            self.download(episode, i, img_bytes)
+            print(f"第{episode}话: 图片{i}抓取完成")
+        print("抓完了")
 
-        next_page(actions)
-        download(episode, i, img_bytes)
-        print(f"第{episode}话: 图片{i}抓取完成")
-    print("抓完了")
+    def decode_base64(self, js, retry_times):
+        try:
+            mg_base64 = driver.execute_script(js).split(',')[1]
+            return base64.b64decode(mg_base64)
+        except:
+            if retry_times > 20:
+                print("失败了")
+                sys.exit()
+            time.sleep(1)
+            retry_times += 1
+            self.decode_base64(js, retry_times)
 
+    def download(self, episode, i, img_bytes):
+        if not os.path.exists(str(episode)):
+            os.makedirs(str(episode))
+        with open(fr"{episode}\{i}.png", 'wb') as f:
+            f.write(img_bytes)
 
-def decode_base64(js, retry_times):
-    try:
-        mg_base64 = driver.execute_script(js).split(',')[1]
-        return base64.b64decode(mg_base64)
-    except:
-        if retry_times > 20:
-            print("失败了")
-            sys.exit()
+    def next_page(self, actions):
+        # 没什么好办法判断完成，睡1秒把
+        # 必须等图片加载完，但是我不知道有什么办法判断图片是否加载完……，网络卡就多抓几次，或者时间调大点
         time.sleep(1)
-        retry_times += 1
-        decode_base64(js, retry_times)
+        actions.click().perform()
 
+    def setup_offset(self):
+        if not self.offset_is_set:
+            actions = ActionChains(driver)
+            actions.move_by_offset(626 * 0.15, 500)
+            self.offset_is_set = True
+            return actions
 
-def download(episode, i, img_bytes):
-    if not os.path.exists(str(episode)):
-        os.makedirs(str(episode))
-    with open(fr"{episode}\{i}.png", 'wb') as f:
-        f.write(img_bytes)
+    def get_total_page(self):
+        return int(re.findall(r"\d / \d.*?(?=<)", driver.page_source)[0].split(r"/")[-1])
 
+    def click_enabled(self, retry_count, element):
+        if retry_count > 20:
+            print("点不了")
+            sys.exit()
 
-def next_page(actions):
-    # 没什么好办法判断完成，睡1秒把
-    time.sleep(1)
-    actions.click().perform()
+        if element.is_enabled():
+            element.click()
+        else:
+            time.sleep(0.5)
+            retry_count = retry_count + 1
+            self.click_enabled(retry_count, element)
 
+    def crawl(self):
+        print("等会")
+        self.load_source(self.episode)
 
-def setup_offset():
-    global offset_is_set
-    if not offset_is_set:
-        actions = ActionChains(driver)
-        actions.move_by_offset(626 * 0.15, 500)
-        offset_is_set = True
-        return actions
-
-
-def get_total_page():
-    return int(re.findall(r"\d / \d.*(?=<)", driver.page_source)[0].split(r"/")[-1])
-
-
-def click_enabled(retry_count, element):
-    if retry_count > 20:
-        print("点不了")
-        sys.exit()
-
-    if element.is_enabled():
-        element.click()
-    else:
-        time.sleep(0.5)
-        retry_count = retry_count + 1
-        click_enabled(retry_count, element)
-
-
-def crawl(episode):
-    print("等会")
-    load_source(episode)
-
-    get_source(episode)
+        self.get_source(self.episode)
