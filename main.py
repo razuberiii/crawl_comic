7db2e606fda283ccabdc8ac08990841d2b21293b
razuberiii <razuberi@163.com>
2023-06-20 20:06:55 +0800

    code update

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import comic
 import ganma
 import ichijin
 
-type = "ichijin"
+type = input("输入抓取网站（comic/ichijin）:")
+name = input("输入漫画名字:")
+episode = input("输入第几话:")
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
 
     if type == "ganma":
-        # ganma的漫画名字在ganma.py里面的find里输入，因为刚写的时候不熟悉python，结构很烂，懒得重构了，把comic的优化了一下
+        # ganma的漫画名字在ganma.py里面的find里输入，因为刚写的时候不熟悉python，结构很烂，懒得重构了
         ganma.crwal_total()
     elif type == "comic":
-        comic = comic.Comic(input("漫画名字:"), input("第几话:"))
+        comic = comic.Comic(name, episode)
         comic.crawl()
 
     elif type == "ichijin":
-        ichijin = ichijin.Ichijin("屑", "2")
+        ichijin = ichijin.Ichijin(name, episode)
         ichijin.crawl()
